8db361ebe3c7f129df07f3cb389f3ee22500c236
ppuya13 <38575594+ppuya13@users.noreply.github.com>
2025-04-29 19:52:15 +0900

    feat: Bolt 회전 추가

--- Assets/Scripts/Item/Bolt.cs
+++ Assets/Scripts/Item/Bolt.cs
@@ -7,15 +7,41 @@ public class Bolt: NetworkBehaviour
 {
     [SerializeField] private GameObject collectEffect;
     [SerializeField] private GameObject bolt;
+    [SerializeField] private GameObject boltMesh;
     
+    [SerializeField] private float spinSpeed = 43f;
+    private bool _isSpinning;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        StartSpin();
+    }
+
+    private void Update()
+    {
+        if (_isSpinning && boltMesh)
+        {
+            // Y축(위 방향)으로 회전
+            boltMesh.transform.Rotate(Vector3.down * (spinSpeed * Time.deltaTime), Space.Self);
+        }
+    }
+
+    private void StartSpin()
+    {
+        _isSpinning = true;
+    }
+
+    private void StopSpin()
+    {
+        _isSpinning = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && NetworkManager.Singleton.IsHost)
         {
-            if (NetworkManager.Singleton.IsHost)
-            {
-                CollectBoltRpc(NetworkObjectId);
-            }
+            CollectBoltRpc(NetworkObjectId);
         }
     }
 
@@ -24,6 +50,7 @@ public class Bolt: NetworkBehaviour
     {
         if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(id, out NetworkObject obj))
         {
+            StopSpin();
             obj.GetComponent<Collider>().enabled = false;
             Bolt boltObj = obj.GetComponent<Bolt>();
             boltObj.bolt.SetActive(false);
